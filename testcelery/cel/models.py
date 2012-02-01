1ff7da2980a4fa4bf253d0d562d2c11521e43a44
Joe Jasinski <jjasinski@imagescape.com>
2012-01-31 18:19:45 -0600

    modifications

--- testcelery/cel/models.py
+++ testcelery/cel/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+STATUSES = ((0, 'Not Started'),
+    (1,'Running'), (2,'Complete'),)
+
+class Contest(models.Model):
+    status = models.IntegerField(choices=STATUSES,  default=0)
+    name = models.CharField(max_length=12)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
